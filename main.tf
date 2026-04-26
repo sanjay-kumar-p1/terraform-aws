32d15af1661afa8259fa0f2fbd1ea1ba96a4d057
sanjay-kumar-p1 <sanjaykumar.p1@outlook.com>
2026-04-26 11:27:50 +0530

    removed eks

--- main.tf
+++ main.tf
@@ -25,19 +25,3 @@ module "ec2" {
   subnet_id = module.vpc.subnet_ids[0]
   sg_id     = module.sg.sg_id
 }
-
-resource "aws_eks_access_entry" "admin" {
-  cluster_name  = aws_eks_cluster.main.name
-  principal_arn = "arn:aws:iam::588376707040:user/awsfree"
-  type          = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "admin_policy" {
-  cluster_name  = aws_eks_cluster.main.name
-  principal_arn = aws_eks_access_entry.admin.principal_arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-
-  access_scope {
-    type = "cluster"
-  }
-}
